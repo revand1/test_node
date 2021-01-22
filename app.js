9b29633bfea82090d8837322fa76b229d1c42161
revand1 <revan.llanos4@gmail.com>
2021-01-22 21:14:06 +0800

    updating env.PORT

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fetch = require('node-fetch');
 const isEmpty = require('lodash.isempty');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -35,6 +36,6 @@ app.post('/',(req,res)=>{
   });
 });
 
-app.listen(80, () => {
+app.listen(PORT, () => {
     console.log('Listening to port 30000');
 });
