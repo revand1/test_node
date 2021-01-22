1ac2958ced43ece3e776a13b38ba305f4192daa8
revand1 <revan.llanos4@gmail.com>
2021-01-22 20:26:46 +0800

    adding start script

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-// const express = require('express');
-// const fetch = require('node-fetch');
-// const isEmpty = require('lodash.isempty');
-// const app = express();
+const express = require('express');
+const fetch = require('node-fetch');
+const isEmpty = require('lodash.isempty');
+const app = express();
 
-// app.use(express.urlencoded({extended:true}));
-// app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 
-// app.post('/',(req,res)=>{
-//     let id = req.body.post.id;
-//     console.log(id);
-//    fetch("https://graphqlzero.almansi.me/api",{
-//     method: 'POST',
-//     headers: {
-//         'Accept-Encoding': 'gzip, deflate, br',
-//         'Content-Type': 'application/json',
-//         'Accept': 'application/json',
-//         'Connection': 'keep-alive',
-//         'DNT': '1',
-//         'Origin': 'https://graphqlzero.almansi.me'
+app.post('/',(req,res)=>{
+    let id = req.body.post.id;
+    console.log(id);
+   fetch("https://graphqlzero.almansi.me/api",{
+    method: 'POST',
+    headers: {
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Connection': 'keep-alive',
+        'DNT': '1',
+        'Origin': 'https://graphqlzero.almansi.me'
         
         
 
-//        },
-//        body: JSON.stringify({"query":"query {\n post(id: "+id+") {\n id\n title\n body\n }\n}"})
+       },
+       body: JSON.stringify({"query":"query {\n post(id: "+id+") {\n id\n title\n body\n }\n}"})
 
-//    })
-//    .then(result => {
-//     return result.json();
-//   })
-//   .then(data => {
-//     console.log("data returned:\n", data);
-//     res.send(data);
-//   });
-// });
+   })
+   .then(result => {
+    return result.json();
+  })
+  .then(data => {
+    console.log("data returned:\n", data);
+    res.send(data);
+  });
+});
 
 app.listen(80, () => {
     console.log('Listening to port 30000');
